f0f61d76c9ceabd076b40821a76783d630452f51
IbrahimHarry <112265863+IbrahimHarry@users.noreply.github.com>
2022-08-28 22:04:59 +0530

    Added add to cart api Added delete from cart api Refactoring updated readme file added nodemon dev dependency

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # shoppingCartApi
+
+Shopping Cart
+ 
+This is a plain api demonstration of shopping cart using nodeJs & Express. It contains shopping 
+cart service for managing the cart. Basic RESTful api for shopping where you can add, list and
+checkpout the products to the cart.
+
+Requirements
+Node.js
+
+Dependencies
+body-parser -> to get data while adding products into the cart
+Express -> It's a back end web application framework for Node.js for handling API's
+
+Dev Dependencies
+nodemon - for restarting the server automatically on the change, also used to debug the application.
+
+For Testing 
+Postman
+
+Available API Routes
+
+Get/products
+Get/getcartitems
+Get/checkout
+Post/addtocart
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const products = [
     { productid: '1', name: 'book', price: 100, minquantity: 3, maximumquanty: null, discountapplyamount: 500, maximumdicount: 60, percentagediscount: 10 },
@@ -7,6 +9,13 @@ const products = [
     { productid: '3', name: 'bag', price: 1500, minquantity: null, maximumquanty: 2, discountapplyamount: null, maximumdicount: null, percentagediscount: null },
 ]
 
+
+function createUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+       return v.toString(16);
+    });
+ }
 function groupBy(objectArray, property) {
     return objectArray.reduce(function (acc, obj) {
         var key = obj[property];
@@ -18,97 +27,75 @@ function groupBy(objectArray, property) {
     }, {});
 }
 
-let cartitems = [
-    { productid: '1', name: 'book', price: 100, minquantity: 3, maximumquanty: null, discountapplyamount: 500, maximumdicount: 60, percentagediscount: 10 },
-    { productid: '1', name: 'book', price: 100, minquantity: 3, maximumquanty: null, discountapplyamount: 500, maximumdicount: 60, percentagediscount: 10 },
-    { productid: '1', name: 'book', price: 100, minquantity: 3, maximumquanty: null, discountapplyamount: 500, maximumdicount: 60, percentagediscount: 10 },
-    { productid: '1', name: 'book', price: 100, minquantity: 3, maximumquanty: null, discountapplyamount: 500, maximumdicount: 60, percentagediscount: 10 },
-    { productid: '1', name: 'book', price: 100, minquantity: 3, maximumquanty: null, discountapplyamount: 500, maximumdicount: 60, percentagediscount: 10 },
-    { productid: '1', name: 'book', price: 100, minquantity: 3, maximumquanty: null, discountapplyamount: 500, maximumdicount: 60, percentagediscount: 10 },
-    { productid: '1', name: 'book', price: 100, minquantity: 3, maximumquanty: null, discountapplyamount: 500, maximumdicount: 60, percentagediscount: 10 },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '3', name: 'bag', price: 1500, minquantity: null, maximumquanty: 2, discountapplyamount: null, maximumdicount: null, percentagediscount: null },
-    { productid: '3', name: 'bag', price: 1500, minquantity: null, maximumquanty: 2, discountapplyamount: null, maximumdicount: null, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-    { productid: '2', name: 'sanitizer', price: 250, minquantity: 10, maximumquanty: null, discountapplyamount: 3000, maximumdicount: 100, percentagediscount: null },
-];
+let cartitems = [];
 
 //Routes
 app.get('/', function (req, res) {
     res.send('Server is running..!');
 });
-
+//available products
 app.get('/products', function (req, res) {
     res.jsonp(products)
 });
-
+//Items in the cart
 app.get('/getcartitems', function (req, res) {
-    res.json(cartitems.length == 0 ? 'No items found!' : cartitems);
+    if (cartitems.length == 0) {
+        res.json('No items found!');
+    } else {
+        res.json(cartitems);
+    }
+});
 
-    // if (cartitems.length == 0) {
-    //     res.json('No items found!');
-    // } else {
-    //     res.json(cartitems);
-    // }
+app.delete('/deletefromcart/:id', function (req, res) {
+    console.log(req.params)
+    if (cartitems.length == 0) {
+        res.json('No items found!');
+    } else {
+
+       var arrayindex=  cartitems.findIndex(x=>x.id == req.params.id);
+
+       if(arrayindex == -1){
+        res.json('Delete failed No items found!');
+       }else{
+        let item = cartitems[arrayindex].id;
+        cartitems.splice(arrayindex,1);
+        res.json('Item with id'+item+ ' is deleted');
+       }
+
+       console.log(cartitems)
+       console.log(arrayindex)
+    }
+
+    res.json('No items found!');
 });
 
 
 app.get('/checkout', function (req, res) {
-    let resss = [];
+    let finalResponse = [];
     let errors = [];
 
-    if (cartitems === 0) {
+    if (cartitems.length === 0) {
         res.json('No items found!');
     } else {
         var groupedproducts = Object.values(groupBy(cartitems, 'productid'));
-
+        
         for (let i = 0; i < groupedproducts.length; i++) {
 
             const productCatgory = groupedproducts[i];
-
             let productcat = {
                 name: productCatgory[0].name,
                 itemPrice: 0,
-                numberofquaNTITY: 0,
+                numberOfQuantity: 0,
                 productid: productCatgory[0].productid,
                 finalPriceafterdiscount: 0
             }
-
             var amount = 0;
             var minquantity = 0;
             var discountprice = 0;
             if (productCatgory.length > 0) {
-
-
-
                 for (let z = 0; z < productCatgory.length; z++) {
                     const product = productCatgory[z];
-
                     amount = amount + product.price;
-
                     minquantity = product.minquantity;
 
                     if (productCatgory.length < product.minquantity) {
@@ -118,7 +105,6 @@ app.get('/checkout', function (req, res) {
                                 msg: "minimum quantity of the product " + product.name + " should be " + product.minquantity
                             });
                         }
-
                     }
 
                     if (product.maximumquanty != null && product.maximumquanty < productCatgory.length) {
@@ -129,20 +115,11 @@ app.get('/checkout', function (req, res) {
                             });
                         }
                     }
-
-
-
                 }
-
                 productcat.itemPrice = amount
-                productcat.numberofquaNTITY = productCatgory.length;
-
-
-                console.log("amount   " + amount)
-
-
+                productcat.numberOfQuantity = productCatgory.length;
                 if (productCatgory[0].discountapplyamount != null && productCatgory[0].discountapplyamount <= amount) {
-                    // dicount eligible 
+                    // discount eligible 
                     //apply discount  
                     // percentagediscount == null when flat rate is there 
 
@@ -166,14 +143,14 @@ app.get('/checkout', function (req, res) {
                 } else {
                     productcat.finalPriceafterdiscount = amount;
                 }
-                resss.push(productcat)
+                finalResponse.push(productcat)
             }
         }
 
         var OBJ = {
             errors: errors,
-            resss: errors.length === 0 ? resss : [],
-            totalsum: errors.length === 0 ? resss.map(i => i.finalPriceafterdiscount).reduce((a, b) => a + b) : 0,
+            finalResponse: errors.length === 0 ? finalResponse : [],
+            totalsum: errors.length === 0 ? finalResponse.map(i => i.finalPriceafterdiscount).reduce((a, b) => a + b) : 0,
             isCouponCodeapplied: false,
             aftercouponappliedamount: 0
         }
@@ -188,15 +165,41 @@ app.get('/checkout', function (req, res) {
 
     }
 
-    res.json(cartitems.length == 0 ? 'No items found!': cartitems);
-    
-    // if (cartitems.length == 0) {
-    //     res.json('No items found!');
-    // } else {
-    //     res.json(cartitems);
-    // }
+    if (cartitems.length == 0) {
+        res.json('No items found!');
+    } else {
+        res.json(cartitems);
+    }
 });
 
+app.post('/addtocart', urlencodedParser, function (req, res) {
+    var product = products.find(x => x.productid == Number.parseInt(req.body.productid));
+    // product.id =  createUUID();
+
+    var obj = {
+        ...product,
+        id: createUUID()
+    }
+
+  
+    if (product) {
+        //add to the cart 
+        cartitems.push(obj);
+        console.log(obj)
+
+        res.json("Product added to cart succesfully!");
+    }
+    else {
+        res.json('Invalid ProductID!');
+    }
+
+})
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 var server = app.listen(8000, function () {
     var host = server.address().address;
     var port = server.address().port;
